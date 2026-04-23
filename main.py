f624359a60ae75570bee6c2f2f7799b7111ed94a
LIU JIAHAO <alivecatliu@gmail.com>
2026-04-23 13:03:28 +0900

    hello world chain

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course!")
+    information = """In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the organization's direction and leadership in the AI boom in the 2020s led him to establish xAI, which became a subsidiary of SpaceX in 2026. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017. In November 2025, a Tesla pay package worth $1 trillion for Musk was approved, which he is to receive over 10 years if he meets specific goals."""
+
+    summary_template = """ give the information {information}  about a person i want you to create：
+    1.A Short summary
+    2.two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    # llm = ChatOpenAI(temperature=0, model="gpt-5-nano")
+    llm = ChatOllama(temperature=0, model="gemma3n")
+    chain = summary_prompt_template | llm
+    response = chain.invoke(input={"information": information}) 
+    print(response.content)
+
+
+if __name__ == "__main__":
+    main()
